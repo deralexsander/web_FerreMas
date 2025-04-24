613b423295a26516f4a778acc68499244c980579
deralexander <nia.munoz@duocuc.cl>
2025-04-23 23:09:04 -0400

    Agrega vistas para inicio, acceso, productos y carrito

--- web_FerreMas/FerreMas/urls.py
+++ web_FerreMas/FerreMas/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('acceso/', views.acceso, name="acceso"),
+    path('productos/', views.productos, name="productos"),
+    path('carrito/', views.carrito, name="carrito"),
 ]
--- web_FerreMas/FerreMas/views.py
+++ web_FerreMas/FerreMas/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'web/inicio.html')
+
+def acceso(request):
+    return render(request, 'registration/acceso.html')
+
+def productos(request):
+    return render(request, 'web/productos.html')
+
+
+def carrito(request):
+    return render(request, 'web/carrito.html')
